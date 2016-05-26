e00d85216e4912ab892788fe47bf8e4c42146543
aimuqianoa059556 <aimuqianoa059556@163.com>
2016-05-26 13:52:42 +0800

    更新数据库配置文件

--- config.inc.php
+++ config.inc.php
@@ -3,9 +3,9 @@
 	define("DRIVER", "pdo");				      					//数据库的驱动，本系统支持pdo(默认)和mysqli两种
 	//define("DSN", "mysql:host=localhost;dbname=yixuncms"); 			//如果使用PDO可以使用，不使用则默认连接MySQL
 	define("HOST", "localhost");			      					//数据库主机
-	define("USER", "root");                               			//数据库用户名
-	define("PASS", "root");                                   			//数据库密码
-	define("DBNAME", "yixuncm");			      						//数据库名
+	define("USER", "ucxGHYrlvj0QsPLz");                               			//数据库用户名
+	define("PASS", "p8qDmtjN9g7wZcbho");                                   			//数据库密码
+	define("DBNAME", "FQpC2ng9cDLeBxNk");			      						//数据库名
 	define("TABPREFIX", "yx_");                          			//数据表前缀
 	define("CTIME", "604800");                         				//缓存时间
 	define("TPLPREFIX", "html");                           			//模板文件的后缀名
